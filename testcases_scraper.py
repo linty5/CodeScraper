512947ff2bff4a7f1261c9b015d6f4503dc71a4d
linty5 <951060525@qq.com>
2023-08-08 21:35:15 +0800

    fix bugs

--- testcases_scraper.py
+++ testcases_scraper.py
@@ -5,13 +5,9 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.service import Service as ChromeService
 
-from utils import convert_text
 
 def get_testcases(info, submission):
 	testcases = {}
